c1af657cf19af23f0f9d93703e1645d64c3e399b
Rogen Barrow <rogenbarrow@hotmail.com>
2022-08-10 14:54:50 -0400

    Deletion

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+import postgres from 'postgres';
+
 const express = require('express')
 const app = express()
 const port = 55;
+
+
+
+const sql = postgres('postgres://postgres:nathifa@host:8033/Client_Registration')
+        .then( () => {
+            console.log('Connected to database')
+        })
+        .catch( (err) => {
+            console.error('Error connecting to the database')
+        })
+
+
+const selectAll = await sql`
+SELECT  *
+FROM    Registration
+`
+
+
 
 app.listen(
     port,
@@ -8,5 +28,5 @@ app.listen(
 );
 
 app.get('/getuser', (req, res) => (
-    res.status(200).send('request received')
+    res.status(200).send({selectAll})
 ));
