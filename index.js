2a193f291f7772773d997f6d09cf2b949703f1a0
Rogen Barrow <rogenbarrow@hotmail.com>
2022-08-10 14:22:34 -0400

    new packages

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const port = 55;
 
 app.listen(
@@ -8,8 +7,6 @@ app.listen(
     console.log(`http://localhost:${port}`)
 );
 
-app.get('getuser', (req, res) (
+app.get('/getuser', (req, res) => (
     res.status(200).send('request received')
-)
-
-)
+));
