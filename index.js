708d6b5efde17d7f5d07a9446ace85cc646b8be8
Rogen Barrow <rogenbarrow@hotmail.com>
2022-08-26 12:03:31 -0400

    New post users

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 55;
+const bodyParser = require('body-parser');
 
 const pgp = require('pg-promise')({});
 const db = pgp('postrgres://postgres:Nathifa@localhost:8033/Client_Registration')
+
+app.use(bodyParser.json());
 
 
 app.listen(
@@ -13,14 +16,31 @@ app.listen(
 
 
 app.get('/getuser',async (req, res) => (
-   await db.any( 'SELECT fullname, username, gender, email FROM Registration')
+   await db.any( `SELECT fullname, username, gender, email FROM Registration`)
         .then((databaseData) => {
-            const [{fullname, username, gender, email}] = databaseData;
+            //const [{fullname, username, gender, email}] = databaseData;
             console.log('Data:', databaseData)
-            res.status(200).send(fullname,  username,  gender, email);
+            res.status(200).send(databaseData);
+            
         })
         .catch((error) => {
             console.log('Error:', error)
+            res.status(400).send('Request not found');
         })
+    ));
+
+app.post('/adduser', (req, res) => (
+    db.any(`INSERT INTO Registration(fullname, username, gender, email) VALUES ('${req.body.fullname}', '${req.body.username}', '${req.body.gender}', '${req.body.email}')`)
+    .then((newUserData) => {
+        // const [{fullname, username, gender, email}] = newUserData;
+        console.log(newUserData);
+        console.log(req.body);
+        res.status(200).send('Username created.')
+    })
+    .catch((error) => {
+        console.log('Error:', error)
+        res.status(400).send('Users not added.') 
+    })
     
-));
+));        
+    
