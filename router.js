a21f00ab8fc54709f1f52d9a4aa515b19749bd27
Rogen Barrow <rogenbarrow@hotmail.com>
2023-02-15 16:03:17 -0400

    added comments

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ const addpassword = require('./handlers/addpassword');
 const searchuser = require('./handlers/searchuser');
 const searchuserbyfirstletter = require('./handlers/searchuserbyfirstletter');
 
-
+//endpoints
 router.get('/ping', (req, res) => ping(req, res));
 router.get('/getuser', (req, res) => getuser(req, res));
 router.post('/adduser', (req, res) => adduser(req, res));
@@ -17,5 +17,4 @@ router.post('/addpassword', (req, res) => addpassword(req, res));
 router.get('/searchuser', (req, res) => searchuser(req, res));
 router.get('/searchuserbyfirstletter', (req, res) => searchuserbyfirstletter(req, res));
            
-
 module.exports = router;
